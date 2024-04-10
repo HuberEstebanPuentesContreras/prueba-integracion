83973f1cd4fd64cb3d7fd2265ae8a2a13a139700
HuberEstebanPuentesContreras <puenteshuber1@gmail.com>
2024-04-10 08:24:50 -0500

    Agregando la clase CustomUsersDetailsService que extiende la configuracion de GrandAuthorities

---
+++ src/main/java/com/example/integracion/security/CustomUsersDetailsService.java
@@ -0,0 +1,39 @@
+package com.example.integracion.security;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.example.integracion.entity.Roles;
+import com.example.integracion.entity.Usuarios;
+import com.example.integracion.iRepository.IUsuariosRepository;
+
+@Service
+public class CustomUsersDetailsService implements UserDetailsService {
+
+	private IUsuariosRepository usuariosRepo;
+	
+	@Autowired
+    public CustomUsersDetailsService(IUsuariosRepository usuariosRepo) {
+        this.usuariosRepo = usuariosRepo;
+    }
+	
+	public Collection<GrantedAuthority> mapToAuthorities(List<Roles> roles){
+        return roles.stream().map(role -> new SimpleGrantedAuthority(role.getName())).collect(Collectors.toList());
+    }
+	
+	@Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Usuarios usuarios = usuariosRepo.findByUsername(username).orElseThrow(() -> new UsernameNotFoundException("Usuario no encontrado"));
+        return new User(usuarios.getUsername(), usuarios.getPassword(), mapToAuthorities(usuarios.getRoles()));
+    }
+}
